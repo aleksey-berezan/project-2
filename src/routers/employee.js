c14b54c06b5ff058f4fc3781f58c515eed319419
Aleksey Berezan <aleksey.berezan@gmail.com>
2018-03-10 14:16:05 -0500

    Adopted new db api in employee.js

--- src/routers/employee.js
+++ src/routers/employee.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const commonutils = require('../utils/commonutils');
-const then = commonutils.then;
+const dbUtil = require('../utils/dbutil');
+const onReady = dbUtil.onReady;
 
 module.exports = router;
 
 // GET
-
 router.get('/', (req, res, next) => {
-    req.db.all(`SELECT * FROM Employee WHERE is_current_employee=1`, then(res, {
+    dbUtil.all(req.db, 'employee', { $is_current_employee: 1 }, onReady(res, {
         callback: (rows) => {
             res.status(200).send({ employees: rows });
         }
@@ -16,16 +15,16 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:id', (req, res, next) => {
-    req.db.get(`SELECT * FROM Employee WHERE id=${req.employeeId}`, then(res, {
-        onNotFound: 404,
-        callback: (row) => {
-            res.status(200).send({ employee: row });
-        }
-    }));
+    dbUtil.get(req.db, 'employee', { $id: req.employeeId },
+        onReady(res, {
+            onNotFound: 404,
+            callback: (row) => {
+                res.status(200).send({ employee: row });
+            }
+        }));
 });
 
 // POST
-
 router.post('/', (req, res, next) => {
     const employee = req.body.employee;
     if (!employee || !employee.name || !employee.position || !employee.wage) {
@@ -33,14 +32,15 @@ router.post('/', (req, res, next) => {
         return;
     }
 
-    req.db.run(`INSERT INTO Employee (name, position, wage) VALUES('${employee.name}','${employee.position}','${employee.wage}')`, then(res, {
-        callback: (_, lastId) => {
-            req.db.get(`SELECT * FROM Employee WHERE id=${lastId}`, then(res, {
-                onNotFound: 404,
-                callback: (row) => {
-                    res.status(201).send({ employee: row });
-                }
-            }));
-        }
-    }));
+    dbUtil.insert(req.db, 'employee', { $name: employee.name, $position: employee.position, $wage: employee.wage },
+        onReady(res, {
+            callback: (_, lastId) => {
+                dbUtil.get(req.db, 'employee', { $id: lastId }, onReady(res, {
+                    onNotFound: 404,
+                    callback: (row) => {
+                        res.status(201).send({ employee: row });
+                    }
+                }));
+            }
+        }));
 });
